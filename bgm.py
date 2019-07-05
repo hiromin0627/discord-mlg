55d3f47828b5a400f8674718bdbd516521e37429
ひろみん <hiromin.work@outlook.jp>
2019-07-05 22:29:06 +0900

    BGM再生用の別ボットを追加

---
+++ bgm.py
@@ -0,0 +1,45 @@
+#coding: utf-8
+#created by @hiromin0627
+
+import discord
+import asyncio
+import configparser
+
+ini = configparser.ConfigParser()
+ini.read('./config.ini', 'UTF-8')
+
+token = ini['tokens']['token-bgm']
+vc_id = int(ini['ids']['vc'])
+bgm_id = int(ini['ids']['bgm-room'])
+log_id = int(ini['ids']['log-room'])
+
+client = discord.Client()
+channel = client.get_channel(vc_id)
+
+@client.event
+async def on_ready():
+    print('---BGM player for MLG v1.0.0---')
+    print('discord.py ver:' + discord.__version__)
+    print('Logged in as ' + client.user.name + '(ID:' + str(client.user.id) + ')')
+    print('Bot created by @hiromin0627')
+    print('--------------------')
+
+@client.event
+async def on_message(message):
+    if not message.author.bot:
+        return
+
+    if message.content.startswith("MLgacha"):
+        vc = await channel.connect()
+        vc.play(discord.FFmpegPCMAudio('./mlg/mlg_bgm.mp3'))
+        while vc.is_playing():
+            await asyncio.sleep(3)
+            
+    if message.content.startswith("disconnect"):
+        if channel.is_connected():
+            vc = channel.voice_client_in()
+        else:
+            vc = await channel.connect()
+        await vc.disconnect()
+
+client.run(token)
---
+++ run_bgm.bat
@@ -0,0 +1,2 @@
+python bgm.py
+exit
